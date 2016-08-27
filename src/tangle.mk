25bc375140aaecebc920b1112e7ff7088d55bd65
pavel <yosifov.pv@gmail.com>
2016-08-27 20:20:42 +0300

    Changed def -> .defs, tokens -> .tokens

--- src/tangle.mk
+++ src/tangle.mk
@@ -22,13 +22,12 @@ SRCNAMES := $(patsubst %.md,%,$(wildcard *.md))
 
 # Goals
 #-------------------------------------------------------------------------------
-.PHONY: all pre clean mk-build-dir tangle
+.PHONY: all do-tangle clean mk-build-dir tangle
 
-all: pre
 
-pre: mk-build-dir $(SRCNAMES) tangle
+all: mk-build-dir $(SRCNAMES) tangle
 
-post: $(SRCNAMES)
+do-tangle: $(SRCNAMES)
 
 clean:
 	$(RM) -rf $(BUILDDIR)
@@ -37,19 +36,19 @@ mk-build-dir:
 	$(MKDIR) $(BUILDDIR)
 
 tangle: $(SRCNAMES)
-	$(MAKE) POST=1 post --no-print-directory
+	$(MAKE) POST=1 do-tangle --no-print-directory
 
 ifndef POST
 $(SRCNAMES):
 	$(eval MD:=$(addsuffix .md,$@))
 	$(MKDIR) $(BUILDDIR)/$(MD)
-	$(MKDIR) $(BUILDDIR)/$(MD)/def
-	$(AWK) -f$(LIBDIR)/tokens.awk $(MD) > $(BUILDDIR)/$(MD)/tokens
-	$(AWK) -vOUTDIR=$(BUILDDIR)/$(MD)/def \
-		-f $(LIBDIR)/defs.awk $(BUILDDIR)/$(MD)/tokens
+	$(MKDIR) $(BUILDDIR)/$(MD)/.def
+	$(AWK) -f$(LIBDIR)/tokens.awk $(MD) > $(BUILDDIR)/$(MD)/.tokens
+	$(AWK) -vOUTDIR=$(BUILDDIR)/$(MD)/.def \
+		-f $(LIBDIR)/defs.awk $(BUILDDIR)/$(MD)/.tokens
 else
 $(SRCNAMES):
 	$(eval MD:=$(addsuffix .md,$@))
-	$(eval REDS:=$(wildcard build/$(MD)/def/*.red))
+	$(eval REDS:=$(wildcard build/$(MD)/.def/*.red))
 	@echo FOUND REDS FOR $@: $(REDS)
 endif
